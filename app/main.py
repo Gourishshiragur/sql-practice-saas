2bd67b20d743750625173bec8eb956976fb9de6b
Gourish Shiragur <gourish.shiragur@gmail.com>
2025-12-26 15:49:19 +0530

    Fix questions import path

--- app/main.py
+++ app/main.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from questions import QUESTIONS
+from app.questions import QUESTIONS
+
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
