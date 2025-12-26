6e36c2e268ef14bfcf14a8b2687a5301b84fde69
Gourish Shiragur <gourish.shiragur@gmail.com>
2025-12-26 15:52:47 +0530

    Fix static and template directory paths

--- app/main.py
+++ app/main.py
@@ -4,10 +4,12 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from app.questions import QUESTIONS
 
-
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
+
+# ✅ FIXED PATHS
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+templates = Jinja2Templates(directory="app/templates")
+
 
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
